c45501dc1830e4aa46c1204f6908dec158d81c39
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-06-17 12:45:55 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	char n;
 
-	for (n = '0'; n < '9'; n++)
+	for (n = '0'; n <= '9'; n++)
 		putchar(n);
 
-	for (n = 'a'; n < 'f'; n++)
+	for (n = 'a'; n <= 'f'; n++)
 		putchar(n);
 
 	putchar('\n');

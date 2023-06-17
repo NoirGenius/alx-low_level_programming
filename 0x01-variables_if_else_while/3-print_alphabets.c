5efa540f7672b7df71da9584c88a76ffe100bf4e
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-06-17 09:52:19 -0700

    adjusted the alphabet code

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
  * main - entry point
+ *
  * Return : 0 is returned
  */
 int main(void)
 {
 	char ch;
+	char CH;
 
 	for (ch = 'a'; ch <= 'z' ; ch++)
-	{
-		if (ch == 'q' || ch == 'a')
-		{
-			continue;
-		}
-			putchar(ch);
-	}
+		putchar(ch);
+
+	for (CH = 'A'; CH <= 'Z' ; CH++)
+		putchar(CH);
+
 	putchar('\n');
 
 	return (0);

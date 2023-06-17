0ce214fb9f370275783a21931b0828b0c5de47d3
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-06-17 09:39:22 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return : 0 is returned
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		if (ch == 'q' || ch == 'a')
+		{
+			continue;
+		}
+			putchar(ch);
+	}
+	putchar('\n');
+}

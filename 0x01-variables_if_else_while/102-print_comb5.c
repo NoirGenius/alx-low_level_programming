034d1d870959752b0f4b116c29c3ad45db9ba0c7
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-06-18 06:53:38 -0700

    reused code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,20 +7,16 @@
  */
 int main(void)
 {
-	int n1, n2;
+	int n;
 
-	for (n1 = 0; n1 < 9; n1++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (n2 = n1 + 1; n2 < 10; n2++)
-		{
-			putchar(n1 + '0');
-			putchar(n2 + '0');
+		putchar(n + '0');
 
-			if (!(n1 == 8 && n2 == 9))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');

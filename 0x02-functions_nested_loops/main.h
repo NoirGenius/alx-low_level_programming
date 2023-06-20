018fffcff72fc3cc3811eaeeae1a50f9373d8f4c
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-06-20 11:52:37 -0700

    just now getting the header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,8 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+int _putchar(char c);
+void print_alphabet(void);
+/* Add any other function prototypes here */
 
+#endif /* MAIN_H */

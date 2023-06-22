aaebcd6276f839d7148e46608ef2bec5cebf9978
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-06-22 13:04:26 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,13 +10,16 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		for (x = 0; x < n; x++)
-		{
-			for (y = 0; y < n; y++)
-				_putchar(' ');
+		_putchar('\n');
+		return;
+	}
 
-			_putchar('\\');
-			_putchar('\n');
-		}
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < x; y++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
 	}
 }

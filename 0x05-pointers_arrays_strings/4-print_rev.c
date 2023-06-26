b84c6aac44c7e6e42df51ae84960d704e6801c1f
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-06-26 10:59:00 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	s--;
+
+	while (len > 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+
+	_putchar('\n');
+}

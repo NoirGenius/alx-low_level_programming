51da2cab09dd72283bca82d2a839b4a53e556eff
NoirGenius_machine <josphatmutinda82@outlook.com>
2023-07-04 11:07:13 -0700

    new header file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - locates a char on a string
+ * @s: pointer to string
+ * @c: xter on a string
+ *
+ * Return: pointer to xter occurence NULL if xter isn't found
+ * 	or NULL if character is found 
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == c)
+		return (s);
+
+	return (NULL);
+}

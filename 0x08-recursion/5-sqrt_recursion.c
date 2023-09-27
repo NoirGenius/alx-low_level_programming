684c1d0c6cfbd011eafd3593ff780df8a3d53a44
NoirGenius <josephatmutinda82@gmail.com>
2023-09-27 13:27:45 -0700

    commit you say

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,31 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Returns the value of x raised to the power of y.
- * @x: The base.
- * @y: The exponent.
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to find the square root of.
  *
- * Return: x^y if y is non-negative, otherwise -1.
+ * Return: The square root of n if it exists, otherwise -1.
  */
-int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	return (sqrt_helper(n, 1));
+}
+
+/**
+ * sqrt_helper - Helper function to find the square root recursively.
+ * @n: The number to find the square root of.
+ * @i: The current guess.
+ *
+ * Return: The square root of n if it exists, otherwise -1.
+ */
+int sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+
+	if (i * i > n)
 		return (-1);
 
-	if (y == 0)
-		return (1);
-
-	return (x * _pow_recursion(x, y - 1));
+	return (sqrt_helper(n, i + 1));
 }
+

ea8f38fa36197dc886ed3fdb7269740a1871c0f2
NoirGenius <josephatmutinda82@gmail.com>
2023-09-27 13:10:44 -0700

    check the comments

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')  // Base case: Check if the current character is the null terminator.
-    {
-        return;  // If it is, return without printing (end of the string).
-    }
+	if (*s == '\0')  /* Base case: Check if the current character ior. */
+	{
+		return;  /* If it is, return without printing (end of the string). */
+	}
 
-    _print_rev_recursion(s + 1);  // Recursively call _print_rev_recursion with the next character.
-    _putchar(*s);  // Print the current character of the string.
+	_print_rev_recursion(s + 1);  /* Recursively call _print */
+	_putchar(*s);  /* Print the current character of the string. */
 }
